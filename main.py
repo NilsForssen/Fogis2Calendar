82b1cf282be8247365cbd01181e0cd02fcd8f2fc
Nils Forssen <forssennils@gmail.com>
2020-05-03 02:14:22 +0200

    Added GUI
    GUI when run without args, not working.

--- main.py
+++ main.py
@@ -5,48 +5,93 @@ import googleCalendar
 import datetime
 import sys
 
+def getDataPage(uName, pWord):
+
+    with requests.Session() as session:
+
+        payload = {
+            "tbAnvandarnamn": pWord,
+            "tbLosenord": uName,
+            "btnLoggaIn": "Logga in"
+        }
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
+        }
+
+        # Log in page
+        logInPage = session.get("https://fogis.svenskfotboll.se/Fogisdomarklient/login/Login.aspx")
+
+        # post with all required information
+        soup = BeautifulSoup(logInPage.content, features="lxml")
+        payload["__VIEWSTATE"] = soup.select_one("#__VIEWSTATE")["value"]
+        payload["__VIEWSTATEGENERATOR"] = soup.select_one("#__VIEWSTATEGENERATOR")["value"]
+
+        payload["__VIEWSTATE"] = soup.find("input", attrs={"name" : "__VIEWSTATE"})["value"]
+        payload["__VIEWSTATEGENERATOR"] = soup.find("input", attrs={"name" : "__VIEWSTATEGENERATOR"})["value"]
+        payload["__EVENTVALIDATION"] = soup.find("input", attrs={"name" : "__EVENTVALIDATION"})["value"]
+
+        session.post("https://fogis.svenskfotboll.se/Fogisdomarklient/login/Login.aspx", data=payload, headers=headers)
+
+        #logInPage = 
+
+        # Schedule is located here
+        dataPage = session.get("https://fogis.svenskfotboll.se/Fogisdomarklient/Uppdrag/UppdragUppdragLista.aspx")
+        print(dataPage.text)
+        print()
+        print()
+        print(logInPage.text)
+        if dataPage.text == logInPage.text:
+
+            # Login unsuccessfull
+
+            return None
+        else:
+            return dataPage
 
 
-try:
-    username = sys.argv[1]
-    password = sys.argv[2]
-except IndexError:
-    print("Both username and password must be passed as arguments")
-    sys.exit()
+def formatGame(game):
+    """
+    Turn given game into google calendar event format
+    """
 
+    # If time is TBD, set event time to 00:00:00
+    if ":" in game["time"][-5:]:
+        startTime =  datetime.datetime.strptime(game["time"][-5:], "%H:%M")
+        duration = datetime.datetime.strptime("01:30", "%H:%M")
+    else:
+        startTime = datetime.datetime.strptime("00:00", "%H:%M")
+        duration = datetime.datetime.strptime("00:00", "%H:%M")
 
+    endTime = startTime + datetime.timedelta(hours=duration.hour, minutes=duration.minute, seconds=duration.second)
+    
+    # create google-calendar-friendly event out of given game
+    gameEvent = {
+      "summary": "Domare {0}".format(game["competition"]),
+      "location": "{0}".format(game["location"].replace("GoogleBingHitta.se", "")),
+      "description": "{0}\nMatchnummer: {1}".format(game["referees"], game["number"]),
+      "start": {
+        "dateTime": "{0}T{1}+02:00".format(game["time"][:10], startTime.strftime("%H:%M:%S"))
+      },
+      "end": {
+        "dateTime": "{0}T{1}+02:00".format(game["time"][:10], endTime.strftime("%H:%M:%S"))
+      },
 
-with requests.Session() as session:
-
-    payload = {
-        "tbAnvandarnamn": username,
-        "tbLosenord": password,
-        "btnLoggaIn": "Logga in"
+      "reminders": {
+        "useDefault": True
+      },
     }
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
-    }
+    return gameEvent
 
-    # Log in page
-    logInPage = session.get("https://fogis.svenskfotboll.se/Fogisdomarklient/login/Login.aspx", headers = headers)
 
-    # post with all required information
-    soup = BeautifulSoup(logInPage.content, features="lxml")
-    payload["__VIEWSTATE"] = soup.select_one("#__VIEWSTATE")["value"]
-    payload["__VIEWSTATEGENERATOR"] = soup.select_one("#__VIEWSTATEGENERATOR")["value"]
-
-    payload["__VIEWSTATE"] = soup.find("input", attrs={"name" : "__VIEWSTATE"})["value"]
-    payload["__VIEWSTATEGENERATOR"] = soup.find("input", attrs={"name" : "__VIEWSTATEGENERATOR"})["value"]
-    payload["__EVENTVALIDATION"] = soup.find("input", attrs={"name" : "__EVENTVALIDATION"})["value"]
-
-    session.post("https://fogis.svenskfotboll.se/Fogisdomarklient/login/Login.aspx", data=payload, headers=headers)
-
-    # Schedule is located here
-    dataPage = session.get("https://fogis.svenskfotboll.se/Fogisdomarklient/Uppdrag/UppdragUppdragLista.aspx")
+def updateCalendar(page):
+    """
+    Update the calendar with games from given dataPage
+    """
 
     # Parse the HTML schedule-table into a list with dictionaries for every coming game 
-    soup = BeautifulSoup(dataPage.content, features="lxml")
+    soup = BeautifulSoup(page.content, features="lxml")
 
     data = []
     gameHeaders = ["time", "competition", "round", "number", "game", "location", "referees", ]
@@ -64,42 +109,6 @@ with requests.Session() as session:
 
     # Remove empty top row
     data.pop(0)
-
-
-    def formatGame(game):
-        """
-        Turn given game into google calendar event format
-        """
-
-        # If time is TBD, set event time to 00:00:00
-        if ":" in game["time"][-5:]:
-            startTime =  datetime.datetime.strptime(game["time"][-5:], "%H:%M")
-            duration = datetime.datetime.strptime("01:30", "%H:%M")
-        else:
-            startTime = datetime.datetime.strptime("00:00", "%H:%M")
-            duration = datetime.datetime.strptime("00:00", "%H:%M")
-
-        endTime = startTime + datetime.timedelta(hours=duration.hour, minutes=duration.minute, seconds=duration.second)
-        
-        # create google-calendar-friendly event out of given game
-        gameEvent = {
-          "summary": "Domare {0}".format(game["competition"]),
-          "location": "{0}".format(game["location"].replace("GoogleBingHitta.se", "")),
-          "description": "{0}\nMatchnummer: {1}".format(game["referees"], game["number"]),
-          "start": {
-            "dateTime": "{0}T{1}+02:00".format(game["time"][:10], startTime.strftime("%H:%M:%S"))
-          },
-          "end": {
-            "dateTime": "{0}T{1}+02:00".format(game["time"][:10], endTime.strftime("%H:%M:%S"))
-          },
-
-          "reminders": {
-            "useDefault": True
-          },
-        }
-
-        return gameEvent
-
 
     # Format all games from fogis
     data = list(map(formatGame, data))
@@ -132,3 +141,66 @@ with requests.Session() as session:
         # Add all new events, in case the event was just deleted, this will just refresh it
         print("Event created at {0}".format(game["start"]["dateTime"]))
         googleCalendar.createEvent(game)
+
+if len(sys.argv) < 2:
+
+    # No arguments passed, launch GUI  prompt for username and password
+    import tkinter as tk
+
+
+    root = tk.Tk()
+
+    root.grid_columnconfigure(1, weight=1)
+
+
+    header = tk.Label(text="Enter your fogis credentials", font='Helvetica 16')
+    uNameLabel = tk.Label(text="Username:", font="Helvetica 10")
+    uNameEntry = tk.Entry()
+    pWordLabel = tk.Label(text="Password:", font="Helvetica 10")
+    pWordEntry = tk.Entry()
+
+    promptString = tk.StringVar()
+    promptString.set("")
+    promptLabel = tk.Label(textvariable=promptString, font="Helvetica 10")
+
+    def btnUpdateCalendar():
+        page = getDataPage(uNameEntry.get(), pWordEntry.get())
+        print(page, uNameEntry.get(), pWordEntry.get())
+        if page:
+            promptString.set("Updated!")
+            promptLabel.config(fg="green2")
+            updateCalendar(page)
+
+            # "Updated"
+
+        else:
+            promptString.set("Login Unsuccessfull!")
+            promptLabel.config(fg="red2")
+            # "Incorrect"
+
+
+    btn = tk.Button(text="Update Calendar", font="Helvetica 10 bold", command=btnUpdateCalendar, bg="green2", activebackground="green2")
+
+    header.grid(columnspan=2, row=0, column=0, sticky="NSEW")
+    uNameLabel.grid(row=1, sticky="W")
+    uNameEntry.grid(row=1, column=1, sticky="EW")
+    pWordLabel.grid(row=2, sticky="W")
+    pWordEntry.grid(row=2, column=1, sticky="EW")
+    promptLabel.grid(columnspan=2, row=3, sticky="NSEW")
+    btn.grid(columnspan=2, row=4)
+
+
+    root.mainloop()
+
+else:
+
+    # username and password arguments passed, don't launch GUI
+
+    try:
+        username = sys.argv[1]
+        password = sys.argv[2]
+    except IndexError:
+        print("Both username and password must be passed as arguments")
+        sys.exit()
+
+    updateCalendar(getDataPage(username, password))
